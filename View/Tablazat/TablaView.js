8c40645721b280df8b22eb562166acea722c4000
BeRobi <screengsm@gmail.com>
2023-11-11 12:28:24 +0100

    javitas

--- View/Tablazat/TablaView.js
+++ View/Tablazat/TablaView.js
@@ -1,6 +1,7 @@
-import { adatLeiro } from "../View/adatLeiro.js";
+import { adatLeiro } from "../adatLeiro.js";
 import FejlecView from "./FejlecView.js";
 import SorView from "./SorView.js";
+
 
 
 export default class TablaView {
