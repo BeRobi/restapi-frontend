bb9eb8230bd635d6f25e448abc0e164d7fe517e6
BeRobi <screengsm@gmail.com>
2023-11-17 22:33:23 +0100

    form

--- View/Tablazat/TablaView.js
+++ View/Tablazat/TablaView.js
@@ -1,3 +1,4 @@
+import UrlapView from "../Urlap/UrlapView.js";
 import { adatLeiro } from "../adatLeiro.js";
 import FejlecView from "./FejlecView.js";
 import SorView from "./SorView.js";
@@ -15,16 +16,23 @@ export default class TablaView {
         </table>`);
     this.tbodyElem = szuloElem.find("tbody");
     this.theadElem = szuloElem.find("thead");
+    this.formElem = $(".urlap");
+    this.gombElem = $("button")
+   
+
+    console.log(this.formElem)
 
     console.log(this.tbodyElem);
+    new UrlapView(adatLeiro, this.formElem);
     new FejlecView(adatLeiro, this.theadElem);
+   
     this.sorMegjelenit();
   }
   sorMegjelenit() {
     // itt példányosítjuk a sort, annyiszor, ahány eleme van
     this.#list.forEach((elem, index) => {
       new SorView(index, elem, this.tbodyElem);
-
+    
       index++;
     });
   }
--- View/Urlap/UrlapView.js
+++ View/Urlap/UrlapView.js
@@ -1,16 +1,59 @@
 import { adatLeiro } from "../adatLeiro.js";
 
-export default class UrlapView{
-    constructor(szuloElem) {
-        this.szuloElem = szuloElem
-        //form tag létrehozása
-        //form elemhez kapaszkodó this.formElem
-        this.htmlOsszerak()
-    }
-    htmlOsszerak(){
-            // végigmegyek az adatleiro kulcsain és létrehozom a beviteli mezőket
+export default class UrlapView {
+  #obj = {};
+  constructor(obj, szuloElem) {
+    this.#obj = obj;
+    this.szuloElem = szuloElem;
 
-            
+    //létrehozok egy form taget a szulőelemben
+    //létrehozok egy kapaszkodót formelemhez this.formElem
+    this.htmlOsszerak();
+  }
+  htmlOsszerak() {
+    /*  végigmegyek az adatLeiro kulcsain és létrehozom az egyes 
+        beviteli mezőket
+         <div class="mb-3 mt-3">
+            <label for="text" class="form-label">fdgdf</label>
+            <input type="text" class="form-control" id="fds" placeholder="Enter email" name="email">
+        </div>
+        */
 
+    /*      let txt = "<form class=`form`>";
+    for (const key in this.#obj) {
+      txt += `<label for="fname">${this.#obj[key]}:</label><br>
+      <input type="text" id="fname" name="fname"><br>`
     }
+    txt += "</form>";
+    console.log(txt);
+    this.szuloElem.append(txt); */
+
+    /*     let txt = "<form class=`form-inline`>";
+    for (const key in this.#obj) {
+      txt += `
+      <label class="sr-only" for="${this.#obj[key]}">Name</label>
+      <input type="text" class="form-control mb-2 mr-sm-2" id="${this.#obj[key]}" placeholder="${this.#obj[key]}">
+    `;
+    }
+    txt += "</form>";
+    console.log(txt);
+    this.szuloElem.append(txt);
+  } */
+
+    let txt = "<form class=`form-inline`>";
+    for (const key in this.#obj) {
+      txt += `<label class="sr-only" for="inlineFormInputGroup${
+        this.#obj[key]
+      }"></label>
+  <div class="input-group mb-2 mr-sm-2">
+    <div class="input-group-prepend"></div>
+    <input type="text" class="form-control" id="inlineFormInputGroup${
+      this.#obj[key]
+    }" placeholder="${this.#obj[key]}"></div>`;
+    }
+
+    txt += `<button type="submit" class="btn btn-primary mb-2">Submit</button></form>`;
+    console.log(txt);
+    this.szuloElem.append(txt);
+  }
 }
